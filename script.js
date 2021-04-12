a2fa472a0b20101ae94825f8c5021fd790f19581
muneebali500 <72123497+muneebali500@users.noreply.github.com>
2021-04-12 11:49:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const daysElement = document.querySelector(`[data-days]`);
+const hoursElement = document.querySelector(`[data-hours]`);
+const minutesElement = document.querySelector(`[data-minutes]`);
+const secondsElement = document.querySelector(`[data-seconds]`);
+
+const countDate = new Date(`May 14, 2021 00:00:00`).getTime();
+
+function countDown() {
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  daysElement.innerText = Math.floor(gap / days);
+  hoursElement.innerText = Math.floor((gap % days) / hours);
+  minutesElement.innerText = Math.floor((gap % hours) / minutes);
+  secondsElement.innerText = Math.floor((gap % minutes) / seconds);
+}
+
+setInterval(countDown);
